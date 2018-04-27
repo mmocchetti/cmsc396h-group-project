c5edb86a0ddd16d1a1851d872c6a73c604fbe3fa
nbathras <nbathras@terpmail.umd.edu>
2018-04-27 00:56:45 -0400

    Update weather_console.py

--- weather_console.py
+++ weather_console.py
@@ -23,13 +23,21 @@ for id,business_id,stars,date in cursor:
     
     business_list.append(individual_business)
     
-print("Review ID:             | Business ID:           | Date of Review      | * | State: |")
-print("-----------------------+------------------------+---------------------+---+--------+---------------------------------------")
+print("Review ID:             | Business ID:           | Date of Review      | * | Temperature: | State: |")
+print("-----------------------+------------------------+---------------------+---+--------------+--------+------------------------------")
     
+cnx2 = mysql.connector.connect(user=mysql_user_config.user, password=mysql_user_config.password, host=mysql_user_config.host, database="yelp_weather_db")
+cursor2 = cnx2.cursor()
+	
 for individual_business in business_list:
     query = ("SELECT id,name,state FROM business WHERE (id='" + individual_business[1] +"')")
     cursor.execute(query)
     
     for id,name,state in cursor:
-        print(individual_business[0] + " | " + str(id) + " | " + individual_business[3] + " | " + individual_business[2] + " |   " + str(state) + "   | " + str(name))
+		#print(individual_business[0] + " | " + str(id) + " | " + individual_business[3] + " | " + individual_business[2] + " |   " + str(state) + "   | " + str(name))
 
+		query = ("SELECT state,myDate,temp FROM weather WHERE (state='" + str(state) + "') AND (myDate='" + individual_business[3] + "')")
+		cursor2.execute(query)
+
+		for state,myDate,temp in cursor2:
+			print(individual_business[0] + " | " + str(id) + " | " + individual_business[3] + " | " + individual_business[2] + " |      " + str(temp) + "      |   " + str(state) + "   | " + str(name))
