becf5d0ee82d2b03d4ece296f744169fca25d6ad
nbathras <nbathras@terpmail.umd.edu>
2018-04-27 00:56:17 -0400

    Update uploadData.py

--- uploadData.py
+++ uploadData.py
@@ -1,39 +1,42 @@
 import mysql.connector
-import test_var
+import mysql_user_config
 
 # Creates the data base if it does not exist, then it creates the table if it does not already exist
-def create_databse():
+def create_databse(cursor):
 	# This creates the database
-	db = mysql.connector.connect(host= test_var.host, user= user.host, password= test_var.password)
-	cursor = db.cursor()
-	cmd = 'CREATE DATABASE IF NOT EXISTS yelp_weather_db'
-	cursor.execute(cmd)
+	#cmd = 'CREATE DATABASE IF NOT EXISTS yelp_weather_db'
+	#cursor.execute(cmd)
 
 	# This will create the table
 	# weather => |state| myDate| temp|
-	cmd = '''CREATE TABLE IF NOT EXISTS weather (
-				state VARCHAR(50),
-				myDate DATE DEF,
-				temp INT
-			'''
-	cursor.execute(sql)
+	cmd = 'CREATE TABLE IF NOT EXISTS weather (state VARCHAR(50),myDate DATE,temp INT)'
+	cursor.execute(cmd)
+	print "Database CREATED!!!"
 
-# Parses the given file
-def read_File(filename):
+db = mysql.connector.connect(user=mysql_user_config.user, password=mysql_user_config.password, host=mysql_user_config.host, database="yelp_weather_db")
+cursor = db.cursor()
 	
+create_databse(cursor)
 
+filepath = 'C:\\Users\\nbathras\\OneDrive\\Documents\\Coding\\Python Coding\\weather_project\\az_2010_2018.txt'  
+with open(filepath) as fp:  
+	line = fp.readline()
+	cnt = 1
+	while line:
+		if cnt < 2:
+			cnt += 1
+			line = fp.readline()
+			pass
+		else:
+			#print("Line {}: {}".format(cnt, line.strip()))
+			#print("Line " + str(cnt) + ": " + line.strip())
+			split_string = line.strip().split(',')
+			date_string = split_string[2][0:5] + "-" + split_string[2][5:7] + "-" + split_string[2][7:9] + " 00:00:00"
+			int_temp = 10
+			print(str(int(float(split_string[3]))) + " " + date_string + " " + split_string[2])
+			insertStatement = "INSERT INTO weather (state, myDate, temp) VALUES ('AZ','" + date_string + "'," + str(int(float(split_string[3]))) + ")"
+			cursor.execute(insertStatement)
+			line = fp.readline()
+			cnt += 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+db.commit()	
