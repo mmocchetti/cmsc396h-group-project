94cf1a1b8f1d5917449331fadaf96b07ad233791
nbathras <nbathras@terpmail.umd.edu>
2018-04-27 01:03:55 -0400

    Update weather_console.py

--- weather_console.py
+++ weather_console.py
@@ -1,14 +1,19 @@
 import mysql.connector
 import sys
 import mysql_user_config
+import unicodedata
+
+#
+# Make sure to run these commands first: we have not automated this system yet
+# use yelp_weather_db;
+# CREATE DATABASE IF NOT EXISTS yelp_weather_db;
+# CREATE TABLE IF NOT EXISTS weather (state VARCHAR(50),myDate DATE,temp INT);
+#
 
 cnx = mysql.connector.connect(user=mysql_user_config.user, password=mysql_user_config.password, host=mysql_user_config.host, database=mysql_user_config.database)
 cursor = cnx.cursor()
 
-#query = raw_input("Type MySQL query here: ")
-#cursor.execute(query)
-
-query = ("SELECT id,business_id,stars,date FROM review LIMIT 100")
+query = ("SELECT id,business_id,stars,date FROM review LIMIT 1000")
 cursor.execute(query)
 
 business_list = list()
@@ -16,8 +21,8 @@ business_list = list()
 for id,business_id,stars,date in cursor:
     individual_business = list()
     #print(str(id) + " | " + str(business_id) + " | " + str(stars) + " | " + str(date))
-    individual_business.append(str(id))
-    individual_business.append(str(business_id))
+    individual_business.append(unicodedata.normalize('NFKD', id).encode('ascii','ignore'))
+    individual_business.append(unicodedata.normalize('NFKD', business_id).encode('ascii','ignore'))
     individual_business.append(str(stars))
     individual_business.append(str(date))
     
@@ -40,4 +45,4 @@ for individual_business in business_list:
 		cursor2.execute(query)
 
 		for state,myDate,temp in cursor2:
-			print(individual_business[0] + " | " + str(id) + " | " + individual_business[3] + " | " + individual_business[2] + " |      " + str(temp) + "      |   " + str(state) + "   | " + str(name))
+			print(individual_business[0] + " | " + unicodedata.normalize('NFKD', id).encode('ascii','ignore') + " | " + individual_business[3] + " | " + individual_business[2] + " |      " + str(temp) + "      |   " + str(state) + "   | " + unicodedata.normalize('NFKD', name).encode('ascii','ignore'))
