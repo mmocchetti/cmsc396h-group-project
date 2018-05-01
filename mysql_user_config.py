46c3b31fe179f258426fdd2d1714ce7711fd9da3
nbathras <nbathras@terpmail.umd.edu>
2018-04-30 20:44:49 -0400

    Update and rename mysql_user_config.py to mysql_user_config_template.py

--- mysql_user_config.py
+++
@@ -1,3 +0,0 @@
-user='user' 
-password='password'
-host='127.0.0.1'
---
+++ mysql_user_config_template.py
@@ -0,0 +1,7 @@
+# You should create a file named mysql_user_config.py 
+# this file should have the fields listed bellow that you use
+# to connect to your mysql server
+
+user='user' 
+password='password'
+host='127.0.0.1'
